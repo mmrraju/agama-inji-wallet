7cf7e6e310f30f84b8afcdb21a55a69caa63953b
Mostafejur Rahman <mrraju.ice.iu@gmail.com>
2025-11-20 03:36:25 +0600

    feat: add AgamaInjiVerificationService.java

---
+++ projects/agama-inji-wallet/lib/org/gluu/agama/inji/AgamaInjiVerificationService.java
@@ -0,0 +1,19 @@
+package org.gluu.agama.inji;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.gluu.agama.inji.AgamaInjiVerificationServiceImpl;
+
+public abstract class AgamaInjiVerificationService{
+
+    public abstract Map<String, Object> verifyServiceURL();
+
+    public abstract String createOpenidRequestUrl(String requestId, String transactionId);
+
+    public abstract Map<String, Object> verifyInjiAppResult(Map<String, String> resultFromapp);
+
+    public static AgamaInjiVerificationService getInstance(){
+        return AgamaInjiVerificationServiceImpl.getInstance();
+    }
+}
